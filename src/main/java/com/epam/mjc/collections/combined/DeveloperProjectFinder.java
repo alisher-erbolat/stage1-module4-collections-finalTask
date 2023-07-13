47e2323d6a84aa40bd379964077debca90a81984
alisher-erbolat <alisher.001017@gmail.com>
2023-07-14 00:47:56 +0600

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,38 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
+            for (String value : e.getValue()) {
+                if (value.equals(developer)) {
+                    list.add(e.getKey());
+                }
+            }
+        }
+        Collections.sort(list);
+        Collections.reverse(list);
+        list.sort(new MyComparator(""));
+
+        return list;
+    }
+}
+
+class MyComparator implements java.util.Comparator<String> {
+
+    private final int referenceLength;
+
+    public MyComparator(String reference) {
+        super();
+        this.referenceLength = reference.length();
+    }
+
+    public int compare(String s1, String s2) {
+        int dist1 = Math.abs(s1.length() - referenceLength);
+        int dist2 = Math.abs(s2.length() - referenceLength);
+
+        return dist2 - dist1;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+
+        for(Map.Entry<String, Integer> e: sourceMap.entrySet()){
+            map.put(e.getKey().length(), new HashSet<>());
+        }
+        for(Map.Entry<String, Integer> e: sourceMap.entrySet()){
+            if (map.containsKey(e.getKey().length())){
+                map.get(e.getKey().length()).add(e.getKey());
+            }
+        }
+        return map;
     }
 }
